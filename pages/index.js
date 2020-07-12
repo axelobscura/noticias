b727754ab7f22694e890c5f27801965703c79120
Axel <axosar@gmail.com>
2020-07-12 11:54:20 -0500

    Test

--- pages/index.js
+++ pages/index.js
@@ -7,24 +7,12 @@ import Head from './layout/head';
 
 import styles from './Index.module.css'
 
-const Index = ({ posts }) => (
+const Index = () => (
     <div>
         <Head />
         <Container fluid className={styles.cardas}>
             <Row className="flex-row flex-nowrap">
-                {posts.store.abril2020.secciones.map((post, index) => (
-                    <Col key={index} xs="12" sm="3" className="p-0 m-1">
-                        <Card className={styles.carta}>
-                            <div className={styles.imagen}>
-                                <Card.Img variant="top" src={post.titulo} />
-                            </div>
-                            <Card.Body className={styles.cuerpo}>
-                                <Card.Title><Link href="/somos"><h2>{post.titulo}</h2></Link></Card.Title>
-                                <Card.Text>{post.titulo}</Card.Text>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                ))}
+                
             </Row>
         </Container>
     </div>
@@ -32,18 +20,16 @@ const Index = ({ posts }) => (
 
 export async function getStaticProps() {
     //const res = await fetch('https://newsapi.org/v2/top-headlines?country=mx&apiKey=d6e94ae6348c44ab9b20c475613aae01')
-    const images = await featch('https://pixabay.com/api/?key=16724135-621b009083b44d598216ac09a&q=construction&image_type=photo&pretty=true');
-    const posts = require('./api/datos.json');
+    //const images = await featch('https://pixabay.com/api/?key=16724135-621b009083b44d598216ac09a&q=construction&image_type=photo&pretty=true');
+    //const posts = require('./api/datos.json');
 
-    console.log("Datos" + posts.store);
-
-    console.log("Datos" + images);
-
+    
     return {
         props: {
             posts,
         },
     }
+    
 }
 
 
